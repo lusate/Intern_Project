a62c4571306e3f0b60a1ccd2500ceaa73f39c495
milvus_lucas <lucas@milvus.co.kr>
2023-04-11 17:35:09 +0900

    업데이트 되었습니다 Spring Boot 2.7.2

--- src/main/java/com/omnilab/template_kotlin/common/AES256.java
+++ src/main/java/com/omnilab/template_kotlin/common/AES256.java
@@ -11,6 +11,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidAlgorithmParameterException;
+import java.security.SecureRandom;
 import java.security.spec.AlgorithmParameterSpec;
 import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
@@ -19,15 +20,17 @@ import org.slf4j.LoggerFactory;
 public class AES256 {
 
 	private static Logger logger = LoggerFactory.getLogger("AES256");
-	private static byte[] ivBytes = "7779328267167869".getBytes(StandardCharsets.UTF_8);
-
 	public static String AES_Encode(String str, String key)	{
 		try{
+			SecureRandom random = new SecureRandom();
+			byte[] bytesIV = new byte[16];
+			random.nextBytes(bytesIV);
+			IvParameterSpec iv = new IvParameterSpec(bytesIV);
+
 			byte[] textBytes = str.getBytes(StandardCharsets.UTF_8);
-			AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
 			SecretKeySpec newKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+			cipher.init(Cipher.ENCRYPT_MODE, newKey, iv);
 			return Base64.encodeBase64URLSafeString(cipher.doFinal(textBytes));
 		}catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
 			logger.error("AES_ENCODE", e);
@@ -37,11 +40,15 @@ public class AES256 {
 
 	public static String AES_Decode(String str, String key)	{
 		try {
+			SecureRandom random = new SecureRandom();
+			byte[] bytesIV = new byte[16];
+			random.nextBytes(bytesIV);
+			IvParameterSpec iv = new IvParameterSpec(bytesIV);
+
 			byte[] textBytes = Base64.decodeBase64(str);
-			AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
 			SecretKeySpec newKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "AES");
 			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-			cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
+			cipher.init(Cipher.DECRYPT_MODE, newKey, iv);
 			return new String(cipher.doFinal(textBytes), StandardCharsets.UTF_8);
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
 			logger.error("AES_DECODE", e);
